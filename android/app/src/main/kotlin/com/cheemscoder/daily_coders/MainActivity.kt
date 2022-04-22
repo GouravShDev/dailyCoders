fb35b07763c994ef66b3271e239a51dcaba5c2cd
GouravShDev <gourav0sharma1@gmail.com>
2022-04-22 18:26:26 +0530

    add basic structures files

---
+++ android/app/src/main/kotlin/com/cheemscoder/daily_coders/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cheemscoder.daily_coders
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:daily_coders/layers/ui/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daily Coders',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
